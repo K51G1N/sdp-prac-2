0fba88ba2b9cff846663dc57c4e3a6302b30dae3
g17e4476 <g17e4476@campus.ru.ac.za>
2020-02-24 21:05:25 +0200

    Added nunit tests for the project

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,18 +1,26 @@
 using NUnit.Framework;
-
+using Problems;
 namespace Problems.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Program a = new Program();
+            Assert.Pass(a.Prefix("hello"), Is.EqualTo("5,1:hello"));
+        }
+        [Test]
+        public void Test2()
+        {
+            Program b = new Program();
+            Assert.Pass(b.Prefix(""), Is.EqualTo("0,0:\"\""));
+        }   
+        [Test]
+        public void Test3()
+        {
+            Program b = new Program();
+            Assert.Pass(b.Prefix("what    ...  did you say?? "), Is.EqualTo("27,5:hello"));
         }
     }
 }
--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+         public String Prefix(String input){
+        String leninput = Convert.ToString(input.Length);
+        String [] words = input.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+        String wlen = Convert.ToString(words.Length);
+        return leninput+","+wlen+":"+input;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String input = Console.ReadLine();
+            Program p = new Program();
+            String result = p.Prefix(input);
+            Console.WriteLine(result);
         }
     }
 }
